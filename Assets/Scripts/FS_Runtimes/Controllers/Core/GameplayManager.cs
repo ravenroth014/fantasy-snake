cbf978bbcbca20749c598d390efd38f634990e25
Atid Puwatnuttasit <atid.puw@gmail.com>
2024-03-06 17:07:58 +0700

    Scenes : Added required scenes

--- Assets/Scripts/FS_Runtimes/Controllers/Core/GameplayManager.cs
+++ Assets/Scripts/FS_Runtimes/Controllers/Core/GameplayManager.cs
@@ -1,9 +1,28 @@
+using System.Threading.Tasks;
 using UnityEngine;
 
 namespace FS_Runtimes.Controllers.Core
 {
     public class GameplayManager : MonoBehaviour
     {
+        #region Fields & Properties
         
+        public static GameplayManager Instance => _instance;
+        private static GameplayManager _instance;
+        
+        #endregion
+
+        #region Methods
+
+        #region Unity Event Methods
+        
+        private void Awake()
+        {
+            _instance = this;
+        }
+        
+        #endregion
+        
+        #endregion
     }
 }
