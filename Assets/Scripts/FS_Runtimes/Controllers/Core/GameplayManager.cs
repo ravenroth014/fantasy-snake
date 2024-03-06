9846b62e11e3445073d57749a72076dba23fd089
Atid Puwatnuttasit <atid.puw@gmail.com>
2024-03-06 16:21:27 +0700

    Manager : Added core managers

---
+++ Assets/Scripts/FS_Runtimes/Controllers/Core/GameplayManager.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+namespace FS_Runtimes.Controllers
+{
+    public class GameplayManager : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+}
---
+++ Assets/Scripts/FS_Runtimes/Controllers/Core/LoaderManager.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+namespace FS_Runtimes.Controllers
+{
+    public class LoaderManager : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+}
