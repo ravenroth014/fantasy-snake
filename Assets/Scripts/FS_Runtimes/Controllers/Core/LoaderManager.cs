ce4c8e2231ce0581e9f1d5042459dc12353fcf2f
Atid Puwatnuttasit <atid.puw@gmail.com>
2024-03-08 09:12:40 +0700

    Loader : Implemented loader manager

--- Assets/Scripts/FS_Runtimes/Controllers/Core/LoaderManager.cs
+++ Assets/Scripts/FS_Runtimes/Controllers/Core/LoaderManager.cs
@@ -1,19 +1,58 @@
+using FS_Runtimes.Utilities;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 namespace FS_Runtimes.Controllers.Core
 {
     public class LoaderManager : MonoBehaviour
     {
-        // Start is called before the first frame update
-        void Start()
-        {
-        
-        }
+        #region Fields & Properties
 
-        // Update is called once per frame
-        void Update()
-        {
+        [Header("Initialize Setting")] 
+        [SerializeField, Tooltip("Target Framerate")] private int _targetFrameRate = 60;
         
+        #endregion
+
+        #region Methods
+
+        #region Unity Event Methods
+        
+        private void Awake()
+        {
+            Init();
+            StartProcess();
         }
+        
+        #endregion
+
+        #region Init Methods
+
+        private void Init()
+        {
+            Application.targetFrameRate = _targetFrameRate;
+        }
+        
+        private void StartProcess()
+        {
+            NavigateToScene(SceneHelper.GameplayScene, false);
+            NavigateToScene(SceneHelper.NavigatorScene, true);
+            SceneManager.UnloadSceneAsync(SceneHelper.LoaderScene);
+        }
+        
+        #endregion
+        
+        #region Load Scene Methods
+        
+        private void NavigateToScene(string sceneName, bool isAdditive)
+        {
+            if (isAdditive == false)
+                SceneManager.LoadSceneAsync(sceneName);
+            else
+                SceneManager.LoadSceneAsync(sceneName, LoadSceneMode.Additive);
+        }
+        
+        #endregion
+        
+        #endregion
     }
 }
---
+++ Assets/Scripts/FS_Runtimes/Utilities/GameHelper.cs
@@ -0,0 +1,9 @@
+namespace FS_Runtimes.Utilities
+{
+    public static class SceneHelper
+    {
+        public static readonly string LoaderScene = "Loader";
+        public static readonly string GameplayScene = "Gameplay";
+        public static readonly string NavigatorScene = "Navigator";
+    }
+}
