dd7af065b07716bcbe43dc711be4d2415e648c61
Atid Puwatnuttasit <atid.puw@gmail.com>
2024-03-08 13:46:20 +0700

    Character setting : Created scriptable object for character setting

---
+++ Assets/Scripts/FS_Runtimes/Utilities/CharacterSetting.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+namespace FS_Runtimes.Utilities
+{
+    [CreateAssetMenu(fileName = "CharacterSetting", menuName = "Settings/Character Setting")]
+    public class CharacterSetting : ScriptableObject
+    {
+        [Header("Character Setting")] 
+        [SerializeField, Tooltip("List of Texture")] private Texture2D[] _textureList;
+
+        public int TextureCount => _textureList is null or { Length: <= 0 } ? 0 : _textureList.Length;
+
+        public Texture2D GetTextureByIndex(int index)
+        {
+            if (index < 0 || index >= TextureCount)
+                return null;
+            return _textureList[index];
+        }
+    }
+}
