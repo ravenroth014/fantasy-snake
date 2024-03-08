fccfc34dca51383e9d4bfc43099c35d263a949cc
Atid Puwatnuttasit <atid.puw@gmail.com>
2024-03-08 23:03:09 +0700

    Input : Added new unity input system package

--- Assets/Scripts/FS_Runtimes/Controllers/Core/GameplayManager.cs
+++ Assets/Scripts/FS_Runtimes/Controllers/Core/GameplayManager.cs
@@ -1,5 +1,6 @@
-using System;
+using FS_Runtimes.Controllers.Level;
 using FS_Runtimes.Controllers.Player;
+using FS_Runtimes.Controllers.Pooling;
 using FS_Runtimes.Utilities;
 using UnityEngine;
 
@@ -8,11 +9,23 @@ namespace FS_Runtimes.Controllers.Core
     public class GameplayManager : MonoBehaviour
     {
         #region Fields & Properties
+
+        public CharacterPooling HeroPooling => _heroPooling;
+        [SerializeField, Tooltip("Hero Pooling")]
+        private CharacterPooling _heroPooling;
+
+        public CharacterPooling EnemyPooling => _enemyPooling;
+        [SerializeField, Tooltip("Enemy Pooling")]
+        private CharacterPooling _enemyPooling;
+
+        public LevelManager LevelManager => _levelManager;
+        [SerializeField, Tooltip("Level Manager")]
+        private LevelManager _levelManager;
         
         public static GameplayManager Instance => _instance;
         private static GameplayManager _instance;
 
-        public CharacterModule Test;
+        // public CharacterModule Test;
         
         #endregion
 
@@ -27,8 +40,8 @@ namespace FS_Runtimes.Controllers.Core
 
         private void Start()
         {
-            if (Test is not null)
-                Test.Init();
+            // if (Test is not null)
+            //     Test.Init();
         }
 
         private void Update()
@@ -40,14 +53,14 @@ namespace FS_Runtimes.Controllers.Core
 
         private void TestCharacterRotation()
         {
-            if (Input.GetKeyDown(KeyCode.W))
-                Test.SetCharacterPosition(EDirection.Up);
-            else if (Input.GetKeyDown(KeyCode.A))
-                Test.SetCharacterPosition(EDirection.Left);
-            else if (Input.GetKeyDown(KeyCode.D))
-                Test.SetCharacterPosition(EDirection.Right);
-            else if (Input.GetKeyDown(KeyCode.S))
-                Test.SetCharacterPosition(EDirection.Down);
+            // if (Input.GetKeyDown(KeyCode.W))
+            //     Test.SetCharacterPosition(EDirection.Up);
+            // else if (Input.GetKeyDown(KeyCode.A))
+            //     Test.SetCharacterPosition(EDirection.Left);
+            // else if (Input.GetKeyDown(KeyCode.D))
+            //     Test.SetCharacterPosition(EDirection.Right);
+            // else if (Input.GetKeyDown(KeyCode.S))
+            //     Test.SetCharacterPosition(EDirection.Down);
         }
         
         #endregion
