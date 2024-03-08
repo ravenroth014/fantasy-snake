f9f0b7694a60b296fd03349cfb7dc887571f2302
Atid Puwatnuttasit <atid.puw@gmail.com>
2024-03-08 09:45:08 +0700

    Game Setting : Create game setting (On Hold)

---
+++ Assets/Scripts/FS_Runtimes/Utilities/GameSetting.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+namespace FS_Runtimes.Utilities
+{
+    [CreateAssetMenu(fileName = "GameSetting", menuName = "Settings/Game Setting")]
+    public class GameSetting : ScriptableObject
+    {
+        // TODO: Create setting fields and properties.
+    }
+}
