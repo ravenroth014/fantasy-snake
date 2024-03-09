9ecc6ede7c644ef26b970c6292f169b6c86985ac
Atid Puwatnuttasit <atid.puw@gmail.com>
2024-03-09 17:12:15 +0700

    State : Implementing game play state.

--- Assets/Scripts/FS_Runtimes/States/GamePlayState.cs
+++ Assets/Scripts/FS_Runtimes/States/GamePlayState.cs
@@ -1,7 +1,34 @@
-﻿namespace FS_Runtimes.States
+﻿using FS_Runtimes.Controllers.Character;
+using FS_Runtimes.Controllers.Level;
+using FS_Runtimes.Controllers.Pooling;
+
+namespace FS_Runtimes.States
 {
     public class GamePlayState : GameState
     {
+        #region Fields & Properties
+
+        private readonly CharacterPooling _heroPooling;
+        private readonly CharacterPooling _enemyPooling;
+        private readonly CharactersManager _charactersManager;
+        private readonly LevelManager _levelManager;
+
+        #endregion
+
+        #region Constructors
+
+        public GamePlayState()
+        {
+            _heroPooling = _gameplayManager.HeroPooling;
+            _enemyPooling = _gameplayManager.EnemyPooling;
+            _charactersManager = _gameplayManager.CharactersManager;
+            _levelManager = _gameplayManager.LevelManager;
+        }
+
+        #endregion
+
+        #region Methods
+        
         public override void OnEnter()
         {
         }
@@ -9,5 +36,7 @@
         public override void OnExit()
         {
         }
+        
+        #endregion
     }
 }
